ab04f395d153188159afc5288067cb37b4d95fd6
Your Name <szoberpatryk@op.pl>
2019-11-07 13:54:09 +0100

    Updates

---
+++ precache-manifest.34dc73a0ccdee337e3dfb511925d5f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4263a9a180216b8af31343f4eb350ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5438b537316667af4c",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5f673e12f25835e322a0",
+    "url": "/static/css/main.ce812231.chunk.css"
+  },
+  {
+    "revision": "ce5438b537316667af4c",
+    "url": "/static/js/2.80ba072e.chunk.js"
+  },
+  {
+    "revision": "5f673e12f25835e322a0",
+    "url": "/static/js/main.999751fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
