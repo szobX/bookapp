5f5dceeaa4b1d1afc04be1d769c89f7f1d595580
Your Name <szoberpatryk@op.pl>
2019-11-07 13:57:36 +0100

    Updates

---
+++ precache-manifest.92f1bf4ca3086e3d550d0a3fd76923ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725906ee765e69d5431fab2d81ea751b",
+    "url": "/bookapp/index.html"
+  },
+  {
+    "revision": "ce5438b537316667af4c",
+    "url": "/bookapp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5f673e12f25835e322a0",
+    "url": "/bookapp/static/css/main.ce812231.chunk.css"
+  },
+  {
+    "revision": "ce5438b537316667af4c",
+    "url": "/bookapp/static/js/2.80ba072e.chunk.js"
+  },
+  {
+    "revision": "5f673e12f25835e322a0",
+    "url": "/bookapp/static/js/main.999751fe.chunk.js"
+  },
+  {
+    "revision": "b996e4c1b48369afda90",
+    "url": "/bookapp/static/js/runtime~main.629da165.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34dc73a0ccdee337e3dfb511925d5f75.js"
+  "/bookapp/precache-manifest.92f1bf4ca3086e3d550d0a3fd76923ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bookapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
